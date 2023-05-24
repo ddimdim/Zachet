14e97c6d6a603df9fbce3d44e318b0630a99b562
ddimdim <123562368+ddimdim@users.noreply.github.com>
2023-05-24 13:50:47 +0500

    Add files via upload

---
+++ Зачет/abx/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace abx
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.newLetterTextBox = new System.Windows.Forms.TextBox();
+            this.newNumberTextBox = new System.Windows.Forms.TextBox();
+            this.newColorTextBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // newLetterTextBox
+            // 
+            this.newLetterTextBox.Location = new System.Drawing.Point(301, 46);
+            this.newLetterTextBox.Name = "newLetterTextBox";
+            this.newLetterTextBox.Size = new System.Drawing.Size(147, 20);
+            this.newLetterTextBox.TabIndex = 4;
+            // 
+            // newNumberTextBox
+            // 
+            this.newNumberTextBox.Location = new System.Drawing.Point(509, 47);
+            this.newNumberTextBox.Name = "newNumberTextBox";
+            this.newNumberTextBox.Size = new System.Drawing.Size(147, 20);
+            this.newNumberTextBox.TabIndex = 5;
+            // 
+            // newColorTextBox
+            // 
+            this.newColorTextBox.Location = new System.Drawing.Point(724, 47);
+            this.newColorTextBox.Name = "newColorTextBox";
+            this.newColorTextBox.Size = new System.Drawing.Size(147, 20);
+            this.newColorTextBox.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(491, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(186, 27);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Добавить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(122, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Вывод списка";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(487, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(256, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Вывод сгруппированного списка";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(313, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Введите букву";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(522, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Введите число";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(736, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 20);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Введите цвет";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(35, 23);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(172, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Выберите что искать";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(460, 148);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(312, 238);
+            this.listBox1.TabIndex = 15;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(51, 148);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(312, 238);
+            this.listBox2.TabIndex = 16;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "буква",
+            "число",
+            "цвет"});
+            this.comboBox1.Location = new System.Drawing.Point(39, 46);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(168, 21);
+            this.comboBox1.TabIndex = 17;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(51, 73);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(145, 27);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Сгруппировать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.newColorTextBox);
+            this.Controls.Add(this.newNumberTextBox);
+            this.Controls.Add(this.newLetterTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox newLetterTextBox;
+        private System.Windows.Forms.TextBox newNumberTextBox;
+        private System.Windows.Forms.TextBox newColorTextBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Зачет/abx/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace abx
+{
+    public partial class Form1 : Form
+    {
+
+        private char[] letters;
+        private int[] numbers;
+        private string[] colors;
+        private List<string> list;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader sr1 = File.OpenText("letters.txt");
+                StreamReader sr2 = File.OpenText("numbers.txt");
+                StreamReader sr3 = File.OpenText("colors.txt");
+                string line1 = sr1.ReadLine();
+                string line2 = sr2.ReadLine();
+                string line3 = sr3.ReadLine();
+                letters = line1.Split(' ').Select(char.Parse).ToArray();
+                numbers = line2.Split(' ').Select(int.Parse).ToArray();
+                colors = line3.Split(' ');
+                list = new List<string>();
+                comboBox1.SelectedIndex = 0;
+                foreach (char letter in letters)
+                {
+                    foreach (int number in numbers)
+                    {
+                        foreach (string color in colors)
+                        {
+                            string entry = $"{{буква = {letter}, число = {number}, цвет = {color}}}";
+                            list.Add(entry);
+                        }
+                    }
+                }
+                Print();
+            }
+            catch
+            {
+                MessageBox.Show("Неверный ввод данных", "Ошибка");
+            }
+            
+        }
+
+
+
+
+
+        private void Print()
+        {
+            listBox2.Items.Clear();
+
+            foreach (string entry in list)
+            {
+                listBox2.Items.Add(entry);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                char newLetter = Convert.ToChar(newLetterTextBox.Text);
+                int newNumber = Convert.ToInt32(newNumberTextBox.Text);
+                string newColor = newColorTextBox.Text;
+              /*  if(listBox1.Items.Contains(newNumber)&& listBox1.Items.Contains(newLetter)&& listBox1.Items.Contains(newLetter))
+                {
+                    MessageBox.Show("Такие данные уже были добавлены", "Ошибка")ж
+                }*/
+                string newEntry = $"{{буква = {newLetter}, число = {newNumber}, цвет = {newColor}}}";
+                list.Add(newEntry);
+
+                Print();
+            }
+            catch
+            {
+                MessageBox.Show("Неверный ввод данных", "Ошибка");
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                listBox1.Items.Clear();
+
+                string vibor = comboBox1.SelectedItem.ToString();
+
+
+                var groupby = list.GroupBy(item =>
+                {
+                    if (vibor == "буква")
+                        return item.Substring(item.IndexOf("буква = "));
+                    if (vibor == "число")
+                        return item.Substring(item.IndexOf("число = "));
+                    if (vibor == "цвет")
+                        return item.Substring(item.IndexOf("цвет = "));
+
+                    return "";
+                });
+                foreach (var group in groupby)
+                {
+                    foreach (string entry in group)
+                    {
+                        listBox1.Items.Add(entry);
+                    }
+
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Неверный ввод данных", "Ошибка");
+            }
+        }
+    }
+}
